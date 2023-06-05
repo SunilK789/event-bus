a9c528e21e6bbf8de36d7827a32d1f402455d9b8
Sunil Kumar <sunilpalarya@gmail.com>
2023-06-05 15:54:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,5 +16,5 @@ app.post('/events',(req, res)=>{
 });
 
 app.listen(4005,()=>{
-    console.log('Listening on 4005');
+    console.log('Event-bus listening on 4005');
 })
