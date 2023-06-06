153dd209f0581107d305cfaa21c3a0efc8aa52b2
Sunil Kumar <sunilpalarya@gmail.com>
2023-06-06 11:20:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ app.use(cors());
 
 app.post('/events',(req, res)=>{
     const event = req.body;
+    console.log('Event Received:', event.type);
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
 
     res.send({status:'OK'});
 });
